191fb1030c0727230b6c97619a5a5eba7495f55a
devLeopar <oktarkara@gmail.com>
2025-03-27 17:16:52 +0300

    feat: shadcn setup, header improvements

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from "react";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { useAuth } from "@clerk/nextjs";
+import Header from "./Header";
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export default function ConvexClientProvider({
@@ -13,7 +14,8 @@ export default function ConvexClientProvider({
 }) {
   return (
     <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-      {children}
+      <Header />
+      <main>{children}</main>
     </ConvexProviderWithClerk>
   );
 }
