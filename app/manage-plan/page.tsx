cc6b8acec064652e7a0d8ff9c3ff50041266748d
devLeopar <oktarkara@gmail.com>
2025-03-31 22:43:45 +0300

    feat: manage plan page setup

---
+++ app/manage-plan/page.tsx
@@ -0,0 +1,16 @@
+import SchematicComponent from "@/components/schematic/SchematicComponent";
+import React from "react";
+
+function ManagePlan() {
+  return (
+    <div className="container xl:max-w-5xl mx-auto p-4 mld:p-0">
+      <h1 className="text-2xl font-bold mb-4 my-8">Manage Your Plan</h1>
+      <p className="text-gray-600 mb-8">Manage your subscription and billing details here.</p>
+      <SchematicComponent
+        componentId={process.env.NEXT_PUBLIC_SCHEMATIC_COMPONENT_ID!}
+      />
+    </div>
+  );
+}
+
+export default ManagePlan;
