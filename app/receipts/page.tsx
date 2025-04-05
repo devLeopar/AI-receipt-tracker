0a802ad95e8158effa104aa402b035dafd5f6f1c
devLeopar <oktarkara@gmail.com>
2025-04-05 15:39:07 +0300

    feat: receiptlist component

--- app/receipts/page.tsx
+++ app/receipts/page.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import PDFDropzone from '@/components/PDFDropzone'
-// import ReceiptsList from '@/components/ReceiptsList'
+import ReceiptsList from '@/components/ReceiptList'
 
 function Receipts() {
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
          <PDFDropzone />
-        {/* <ReceiptsList />  */}
+        <ReceiptsList /> 
       </div>
     </div>
   )
